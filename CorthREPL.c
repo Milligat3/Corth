6f5ae9e8255878bc2d5ff9e68cfb81512a587fc9
Milligat3 <maxik20062011@gmail.com>
2025-08-18 22:11:33 +0500

    Added JSR/RTS, Stack Frames, Data Stack

--- CorthREPL.c
+++ CorthREPL.c
@@ -1,5 +1,7 @@
 // StackBasedVM.c
-
+// MAIN TODO: БЛЯТЬ! ДРУЖИЩЕ! СМОТРИ, ЧТОБЫ НАХУЙ СДЕЛАТЬ ФРЕЙМЫ, ЗАСЕКАЙ БЛЯТЬ, МЫ ПУШИМ FP НА CALL_STACK, 
+// А ПРИ RTS ЕГО ВОССТАНАВЛИВАЕМ! ВСЕ ПРОСТО ЕБИТ ТВОЮ МАТЬ ХАХАХХ! ДАВАЙ, ЗАВТРА ЗА РАБОТУ ПОСЛЕ ГАШНИ
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,9 +12,9 @@
 
 typedef struct
 {
-	size_t sp;
-	int32_t stack[STACK_SIZE];
-	uint32_t PC, size;
+	size_t sp, fp, csp, dp, fdp;
+	int32_t stack[STACK_SIZE], data_stack[STACK_SIZE];
+	uint32_t PC, size, call_stack[STACK_SIZE];
 	uint8_t* program;
 }stack_t;
 
@@ -24,17 +26,19 @@ void push(stack_t* VM, int32_t n)
 	if(VM->sp >= STACK_SIZE)
 	{
 		printf("Stack Overflow. Aborting.\n");
-		return;
+		free(VM->program);
+		exit(1);
 	}
 	VM->stack[VM->sp++] = n;
 }
 
 int32_t pop(stack_t* VM)
 {
-	if(VM->sp == 0)
+	if(VM->sp <= VM->fp)
 	{
 		printf("Stack Underflow. Aborting.\n");
-		return 0;
+		free(VM->program);
+		exit(1);
 	}
 	int32_t to_ret = VM->stack[--VM->sp];
 	return to_ret;
@@ -42,10 +46,11 @@ int32_t pop(stack_t* VM)
 
 void add(stack_t* VM)
 {
-	if(VM->sp < 2)
+	if(VM->sp - VM->fp < 2)
 	{
 		printf("Cannot execute addition. Not enough operands.\n");
-		return;
+		free(VM->program);
+		exit(1);
 	}
 	int32_t a = pop(VM);
 	int32_t b = pop(VM);
@@ -54,10 +59,11 @@ void add(stack_t* VM)
 
 void sub(stack_t* VM)
 {
-	if(VM->sp < 2)
+	if(VM->sp - VM->fp < 2)
 	{
 		printf("Cannot execute substraction. Not enough operands.\n");
-		return;
+		free(VM->program);
+		exit(1);
 	}
 	int32_t a = pop(VM);
 	int32_t b = pop(VM);
@@ -66,10 +72,11 @@ void sub(stack_t* VM)
 
 void mul(stack_t* VM)
 {
-	if(VM->sp < 2)
+	if(VM->sp - VM->fp < 2)
 	{
 		printf("Cannot execute multiplication. Not enough operands.\n");
-		return;
+		free(VM->program);
+		exit(1);
 	}
 	int32_t a = pop(VM);
 	int32_t b = pop(VM);
@@ -78,10 +85,11 @@ void mul(stack_t* VM)
 
 void divis(stack_t* VM)
 {
-	if(VM->sp < 2)
+	if(VM->sp - VM->fp < 2)
 	{
 		printf("Cannot execute division. Not enough operands.\n");
-		return;
+		free(VM->program);
+		exit(1);
 	}
 	int32_t a = pop(VM);
 	int32_t b = pop(VM);
@@ -90,10 +98,11 @@ void divis(stack_t* VM)
 
 void right_shift(stack_t* VM)
 {
-	if(VM->sp < 1)
+	if(VM->sp - VM->fp < 1)
 	{
 		printf("Cannot execute shift. Not enough operands.\n");
-		return;
+		free(VM->program);
+		exit(1);
 	}
 	int32_t num = pop(VM);
 	push(VM, num >> 1);
@@ -101,10 +110,11 @@ void right_shift(stack_t* VM)
 
 void left_shift(stack_t* VM)
 {
-	if(VM->sp < 1)
+	if(VM->sp - VM->fp < 1)
 	{
 		printf("Cannot execute shift. Not enough operands.\n");
-		return;
+		free(VM->program);
+		exit(1);
 	}
 	int32_t num = pop(VM);
 	push(VM, num << 1);
@@ -112,10 +122,11 @@ void left_shift(stack_t* VM)
 
 void inc(stack_t *VM)
 {
-	if(VM->sp < 1)
+	if(VM->sp - VM->fp < 1)
 	{
 		printf("Cannot execute increment. Not enough operands.\n");
-		return;
+		free(VM->program);
+		exit(1);
 	}
 	int32_t a = pop(VM);
 	a++;
@@ -124,10 +135,11 @@ void inc(stack_t *VM)
 
 void dec(stack_t *VM)
 {
-	if(VM->sp < 1)
+	if(VM->sp - VM->fp < 1)
 	{
 		printf("Cannot execute decrement. Not enough operands.\n");
-		return;
+		free(VM->program);
+		exit(1);
 	}
 	int32_t a = pop(VM);
 	a--;
@@ -136,10 +148,11 @@ void dec(stack_t *VM)
 
 void dup(stack_t *VM)
 {
-	if(VM->sp < 1)
+	if(VM->sp - VM->fp < 1)
 	{
 		printf("Cannot execute duplication. Not enough operands.\n");
-		return;
+		free(VM->program);
+		exit(1);
 	}
 	int32_t a = pop(VM);
 	push(VM, a);
@@ -148,10 +161,11 @@ void dup(stack_t *VM)
 
 void swp(stack_t *VM)
 {
-	if(VM->sp < 2)
+	if(VM->sp - VM->fp < 2)
 	{
 		printf("Cannot execute swap. Not enough operands.\n");
-		return;
+		free(VM->program);
+		exit(1);
 	}
 	int32_t a = pop(VM);
 	int32_t b = pop(VM);
@@ -159,12 +173,13 @@ void swp(stack_t *VM)
 	push(VM, b);
 }
 
-void peek(stack_t* VM)
+void out(stack_t* VM)
 {
-	if(VM->sp < 1)
+	if(VM->sp - VM->fp < 1)
 	{
-		printf("Cannot peek. Not enough operands.\n");
-		return;
+		printf("Cannot print. Not enough operands.\n");
+		free(VM->program);
+		exit(1);
 	}
 	int32_t a = pop(VM);
 	push(VM, a);
@@ -174,6 +189,111 @@ void peek(stack_t* VM)
 void jmp(stack_t *VM, uint32_t addr)
 {
 	VM->PC = addr;
+}
+
+void call_st_push(stack_t* VM, uint32_t addr)
+{
+	if(VM->csp >= STACK_SIZE)
+	{
+		printf("Call Stack Overflow. Aborting.\n");
+		free(VM->program);
+		exit(1);
+	}
+	VM->call_stack[VM->csp++] = addr;
+}
+
+uint32_t call_st_pop(stack_t* VM)
+{
+	if(VM->csp == 0)
+	{
+		printf("Call Stack Underflow. Aborting.\n");
+		free(VM->program);
+		exit(1);
+	}
+	int32_t addr = VM->call_stack[--VM->csp];
+	return addr;
+}
+
+void jsr(stack_t *VM, uint32_t addr, uint8_t num_of_args)
+{
+	if(num_of_args > VM->sp)
+	{
+		printf("Not enough arguments on the stack.\n");
+		free(VM->program);
+		exit(1);
+	}
+	int32_t num = pop(VM);
+	push(VM, num);
+	call_st_push(VM, VM->PC);
+	call_st_push(VM, VM->fp);
+	call_st_push(VM, VM->fdp);
+	VM->fp = VM->sp;
+	VM->fp -= num_of_args;
+	VM->fdp = VM->dp;
+	VM->PC = addr;
+}
+
+void rts(stack_t *VM)
+{
+	int32_t ret_num = pop(VM);
+
+	VM->sp = VM->fp;
+	VM->dp = VM->fdp;
+	push(VM, ret_num);
+
+	VM->fdp = call_st_pop(VM);
+	VM->fp = call_st_pop(VM);
+	VM->PC = call_st_pop(VM);
+
+}
+
+void store(stack_t *VM)
+{
+	if(VM->sp - VM->fp < 1)
+	{
+		printf("Cannot store. Not enough operands on the stack.\n");
+		free(VM->program);
+		exit(1);
+	}
+	int32_t to_push = pop(VM);
+	VM->data_stack[VM->dp++] = to_push;
+}
+
+void peek_data(stack_t *VM)
+{
+	if(VM->sp - VM->fp < 1)
+	{
+		printf("Cannot peek at variable. Not enough operands on the stack.\n");
+		free(VM->program);
+		exit(1);
+	}
+	int32_t index = VM->fdp + pop(VM);
+	if(index >= VM->dp)
+	{
+		printf("No such variable by that index(%d) in variable stack.\n", index);
+		free(VM->program);
+		exit(1);
+	}
+	int32_t to_push = VM->data_stack[index];
+	push(VM, to_push);
+}
+
+void poke_data(stack_t *VM)
+{
+	if(VM->sp - VM->fp < 2)
+	{
+		printf("Cannot poke at variable. Not enough operands on the stack.\n");
+		free(VM->program);
+		exit(1);
+	}
+	int32_t index = VM->fdp + pop(VM);
+	if(index >= VM->dp)
+	{
+		printf("No such variable by that index(%d) in variable stack.\n", index);
+		free(VM->program);
+		exit(1);
+	}
+	VM->data_stack[index] = pop(VM);
 }
 
 int only_digits(char* str)
@@ -295,12 +415,63 @@ void print_stack(stack_t* VM)
 	}
 }
 
+void print_call_stack(stack_t* VM)
+{
+	for(int i = 0; i < VM->csp; i++)
+	{
+		printf("%s%d%s", i == 0 ? "[\"" : " \"", VM->call_stack[i], i == VM->csp-1 ? "\"]\n\n" : "\",");
+	}
+}
+void print_var_stack(stack_t* VM)
+{
+	for(int i = 0; i < VM->dp; i++)
+	{
+		printf("%s%d%s", i == 0 ? "[\"" : " \"", VM->data_stack[i], i == VM->dp-1 ? "\"]\n\n" : "\",");
+	}
+}
+void debug_output(stack_t *VM)
+{
+	printf("\nDATA STACK:\n");
+	print_stack(VM);
+	printf("CALL STACK:\n");
+	print_call_stack(VM);
+	printf("VAR STACK:\n");
+	print_var_stack(VM);
+	printf("Current state: SP: %zu, FP: %zu, CSP: %zu, DP: %zu, FDP: %zu, PC: %d\n\n", VM->sp, VM->fp, VM->csp, VM->dp, VM->fdp, VM->PC);
+
+}
+char opcodes[][32] = {
+	"OP_UNKNOWN",
+	"OP_PSH",
+	"OP_POP",
+	"OP_ADD",
+	"OP_SUB",
+	"OP_DIV",
+	"OP_MUL",
+	"OP_RSH",
+	"OP_LSH",
+	"OP_JMP",
+	"OP_JNZ",
+	"OP_JZ",
+	"OP_JSR",
+	"OP_RTS",
+	"OP_DUP",
+	"OP_SWP",
+	"OP_INC",
+	"OP_DEC",
+	"OP_OUT",
+	"OP_STR",
+	"OP_WRD",
+	"OP_RDD",
+	"OP_HLT"
+};
+
 void exec_opcodes(stack_t *VM)
 {
 	while(VM->PC < VM->size)
 	{
 		
-		printf("prog[0x%X]: 0x%X\n", VM->PC, VM->program[VM->PC]);
+		//printf("prog[0x%X]: %s\n", VM->PC, opcodes[VM->program[VM->PC]]);
 		switch(VM->program[VM->PC])
 		{
 			case OP_PSH:
@@ -391,6 +562,25 @@ void exec_opcodes(stack_t *VM)
 				}
 				break;
 			}
+			case OP_JSR:
+			{
+				VM->PC++;
+				int32_t addr = (VM->program[VM->PC]) | (VM->program[VM->PC + 1] << 8) | (VM->program[VM->PC + 2] << 16) | (VM->program[VM->PC + 3]  << 24);
+				VM->PC += 4;
+				uint8_t num_of_args = (VM->program[VM->PC++]);
+				jsr(VM, addr, num_of_args);
+				break;
+			}
+			case OP_RTS:
+			{
+				rts(VM);
+				break;
+			}
+			case OP_HLT:
+			{
+				debug_output(VM);
+				return;
+			}
 			case OP_INC:
 			{
 				VM->PC++;
@@ -415,19 +605,40 @@ void exec_opcodes(stack_t *VM)
 				swp(VM);
 				break;
 			}
-			case OP_PEK:
+			case OP_OUT:
 			{
 				VM->PC++;
-				peek(VM);
+				out(VM);
 				break;
 			}
+			case OP_STR:
+			{
+				VM->PC++;
+				store(VM);
+				break;
+			}
+			
+			case OP_WRD:
+			{
+				VM->PC++;
+				poke_data(VM);
+				break;
+			}
+			
+			case OP_RDD:
+			{
+				VM->PC++;
+				peek_data(VM);
+				break;
+			}
+			
 			default:
 
 				printf("UNKNOWN OPCODE: 0x%X\n", VM->program[VM->PC]);
 				VM->PC++;
 				break;
 		}
-		print_stack(VM);
+		// debug_output(VM);
 	}
 }
 
