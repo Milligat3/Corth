4e373a6350c21889ff139db90599b0208b2d9371
Milligat3 <maxik20062011@gmail.com>
2025-11-25 00:01:47 +0500

    Little fixes

--- 6502trans/lexical_analysys.c
+++ 6502trans/lexical_analysys.c
@@ -232,7 +232,7 @@ void push_str_tkn(tokenizer_t* tknzr, token_t tkn_t)
 	{
 		tkn++;
 		unsigned long long int to_p = strtoul(tkn, NULL, 16);
-		tkn = itoa(to_p, tkn, 10);
+		sprintf(tkn, "%llu", to_p);
 		size = strlen(tkn);
 		strncpy(tknzr->tokens[tknzr->size].tkn_str, tkn, size);
 		tknzr->tokens[tknzr->size].tkn_str[size] = '\0';
